3bac4412598725048d89dd6f8ca045dc9f34fbe7
alexmoiseyev <keysergame24@gmail.com>
2023-07-27 13:20:29 +0300

    pioner__code

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.menu__burger').click(function(){
+        $('.menu__burger').toggleClass('open-menu');
+        $('.menu__burger').toggleClass('open-menu');
+    });
+});
